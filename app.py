6d68a7942552d3c9b3bf289ab3b54da3f4306fea
Chiemeka Alim <lord.chiemy@gmail.com>
2017-06-30 15:34:25 +0100

    Make use of sqlalchemy automap for models

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres@localhost:5432/cis"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+import models as model
+
 api = Api(app)
 
+
+# Service Types
+class ServiceTypesList(Resource):
+    def get(self):
+        service_types_list = model.session.query(model.ServiceType).all()
+
+        result = []
+        for service_type in service_types_list:
+            obj = {
+                'id': service_type.id,
+                'description': service_type.description
+            }
+
+            result.append(obj)
+
+        response = jsonify(result)
+        response.status_code = 200
+        return response
+
+
+api.add_resource(ServiceTypesList, '/stl')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# Making use of sqlalchemy automap
+
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -20,23 +22,5 @@ Service = Base.classes.service
 CustomerAddress = Base.classes.customer_address
 ServiceLocation = Base.classes.service_location
 
-#
+
 session = Session(engine)
-
-
-# rudimentary relationships are produced
-# session.add(Organization_Unit(organization_business_id=3, name="Unit 3", description="This is Org Unit 3"))
-# session.add(Customer(customer_id=3, organization_business_id=3, name="Taiye", gender="female"))
-# session.add(Address(address_id=3, address_line_1="13th Ave, Portsmouth", address_line_2="23th Ave, Kinshasha", zip_code=31232, billing=False))
-# session.add(ServiceType(id=3, description="This is a third public service announcement"))
-# session.add(Service(service_id=3, service_type_id=3, provider_id=3, description="The main service rendered part 3"))
-# session.add(CustomerAddress(id=3, customer_id=3, address_id=3))
-# session.add(ServiceLocation(id=3, service_id=3, address_id=3, latitude=112.721, longitude=203.201))
-
-# session.commit()
-
-# # collection-based relationships are by default named
-# # "<classname>_collection"
-# c1 = session.query(ServiceType).first()
-# print(c1.description)
-# # print (c1.address_collection)
